c8c7399beae1a2d89f1c013655ca1e62268edc6e
Rahul33360 <109918304+Rahul33360@users.noreply.github.com>
2025-04-09 15:34:29 +0530

    template literlas

---
+++ Telusko/literlas.java
@@ -0,0 +1,15 @@
+
+public class literlas {
+    public static void main (String args[]) {
+        char c = 'a';
+        c++;
+        System.out.println(c);  // b
+        c++;
+        System.out.println(c);  //  c
+        // it means we can increment characters in java
+
+      
+
+
+    }
+}
