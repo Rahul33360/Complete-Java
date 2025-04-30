fc9f078f0b9b3b65c54ce923402a710073d6e1cf
Rahul33360 <109918304+Rahul33360@users.noreply.github.com>
2025-04-30 12:07:43 +0530

    annotations

--- Telusko/annotations.java
+++ Telusko/annotations.java
@@ -1,5 +1,6 @@
 //   Java provides several built-in annotations, such as @Deprecated, @Override, and @SuppressWarnings
-
+//   @Deprecated  --> this means that the current version will no longer be available in the future. It is
+//   present now but will be removed in future.
 class Anno {
     public void showingTheOutputInScreenWithText() {
         System.out.println("in Anno Show");
