e5831aa7fc3f566158b3a76325a3285aa47fdc9d
Rahul33360 <109918304+Rahul33360@users.noreply.github.com>
2025-05-02 12:09:04 +0530

    different_type_Exceptions

--- Telusko/Exceptions.java
+++ Telusko/Exceptions.java
@@ -8,7 +8,7 @@
 
 public class Exceptions {
     public static void main(String[] args) {
-        int i = 2;
+/*      int i = 0;
         int j = 12;
 //        this is the we handel any error or exceptions. First code will be executed in try block and if it got any error
 //        it will move to catch block and run that block of code.
@@ -20,6 +20,60 @@ public class Exceptions {
         }
         System.out.println("j = " + j);
         System.out.println("try catch ends here");
+*/
+
+        // some more with try and catch
+        //  There are a lots of ways (of exception handling) in specific ways.Like AirthmethicExcetption, ArrayOutOfBOund,
+        //  NullPointerException , etc. All these are child Classes of Exception (e), parent class. So if we using
+        //  a lots of Exceptions (Like these Like AirthmethicExcetption, ArrayOutOfBOund ...) , then in that case the
+        //  " Excetption " always will be written in the last (if it is written).
+        ///  A point where a block of code will get Excetption, it stops the execution at that
+        ///  point and move to catch block without executing the next line of the try block.
+
+/*        int i = 0;
+        int j = 12;
+
+        try{
+            j = 12/i;
+            System.out.println("Execution Stopped if previous line gets Excetption. And this line won't be printed");
+            // You can run this block and check the above statement won't get printed.
+        }
+        catch (Exception e) {     // if there doesn't happen any error, this block of code completely skipped.
+            System.out.println("Exception hit. Something Wrong " +e);
+        }
+        System.out.println("j = " + j);
+        System.out.println("try catch ends here");
+*/
+        //  let's see different types of Excetptionsss 😀 .
+        int i = 0;
+        int j = 12;
+        String null_Str = null;
+        int []arr = new int[5];
+        try{    // the first Exception only will be executed rest stopped.
+//            j = 12/i;  //if at this point it throws Excetption. Execution stops here.will move to Arithmetic Excetption block.
+//            System.out.println(null_Str.length());  // if this throws Excetption.NullPointerException this will be executed.
+            System.out.println("Accesing Null Value from Array" + arr[5]);  // at 5 null value is there.
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Out Of bound" + e);
+        }
+        catch (NullPointerException n) {
+            System.out.println("Null Value" + n);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Arithmetic Error" + e);
+        }
+        catch (Exception e) {     // if there doesn't happen any error, this block of code completely skipped.
+            System.out.println("Exception hit. Something Wrong " +e);
+        }   // this catch block will be executed when none of the above Exception match with anyone. To understand
+        // it, You can think like it is default case. It will catch when no one will. Just like we have we "Switch" case
+        // where there are some cases and we make a default case if none of the mentioned cases match it will
+        // execute the default part code block.
+        
+
+        System.out.println("j = " + j);
+        System.out.println("try catch ends here");
+
 
 
     }
